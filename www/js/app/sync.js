e835f556d613e5cbb2c434c717014a3ed4d7671f
Chris Anderson <jchris@apache.org>
2013-03-26 16:54:25 -0700

    browser id login works properly

--- www/js/app/sync.js
+++ www/js/app/sync.js
@@ -24,10 +24,10 @@ var pullRep = {
   };
 
 // takes care of triggering pull and push replication to the cloud.
-// also handles getting a browserid assertion if there is an authentication error.
+// also handles getting a persona assertion if there is an authentication error.
 // is a sync is running it will cancel it and retrigger transparently.
 function triggerSync(cb, retries) {
-  if (retries === 0) cb("too many retries");
+  if (retries === 0) return cb("too many retries");
   retries = retries || 3;
   console.log(["triggering sync", pullRep]);
   refreshSync(pushRep, function(err, ok) {
@@ -36,15 +36,23 @@ function triggerSync(cb, retries) {
     // or a sync replication API
     setTimeout(function(){
       config.dbServer.get("_active_tasks", function(err, tasks){
-        var info = tasks[0];
+        if (tasks.length == 0) {
+          return cb('replication not running');
+        }
+        var needsLogin = true;
+        for (var i = 0; i < tasks.length; i++) {
+          if (!tasks[i].error || tasks[i].error[0] != 401) {
+            needsLogin = false;
+          }
+        };
         console.log(["_active_tasks", tasks]);
-        if (info.error && info.error[0] == 401) {
-          window.presentBrowserIdDialog(config.syncOrigin, function(err, assertion){
+        if (needsLogin) {
+          window.presentPersonaDialog(config.syncOrigin, function(err, assertion){
             if (err) throw (err);
             // we are logged in!
             // todo we should make sure the email address is the same as our content belongs to
             var postbody = {assertion:assertion};
-            config.dbServer.post("_browserid_assertion", postbody, function(err, info) {
+            config.dbServer.post("_persona_assertion", postbody, function(err, info) {
               if (err) throw(err);
               if (info.email) {
                 config.db.get("_local/user", function(err, user) {
@@ -52,8 +60,8 @@ function triggerSync(cb, retries) {
                     config.db.post({_id : "_local/user", email:info.email}, function(err, ok){
                       if (err) throw(err);
                       // happiness
-                      pullRep.source.auth = {browserid:{email:info.email}};
-                      pushRep.target.auth = {browserid:{email:info.email}};
+                      pullRep.source.auth = {persona:{email:info.email}};
+                      pushRep.target.auth = {persona:{email:info.email}};
                       console.log(["retry with email", info.email]);
                       triggerSync(cb, retries-1);
                     });
@@ -62,8 +70,8 @@ function triggerSync(cb, retries) {
                       cb("this device is already synced for "+user.email);
                     } else {
                       // happiness is copy/paste :)
-                      pullRep.source.auth = {browserid:{email:info.email}};
-                      pushRep.target.auth = {browserid:{email:info.email}};
+                      pullRep.source.auth = {persona:{email:info.email}};
+                      pushRep.target.auth = {persona:{email:info.email}};
                       console.log(["retry with email", info.email]);
                       triggerSync(cb, retries-1);
                     }
